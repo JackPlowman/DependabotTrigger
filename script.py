4095f9c1c44335d6dbd8047632d794cfab71041c
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-03-16 19:11:51 +0000

    update

---
+++ script.py
@@ -0,0 +1,13 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "playwright==1.50.0",
+# ]
+# ///
+
+def main() -> None:
+  print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
