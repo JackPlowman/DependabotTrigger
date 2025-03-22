0782c6b44ca015b5945cb5a504e775297c5da377
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-03-22 22:16:40 +0000

    update

--- src/app.py
+++ src/app.py
@@ -1,13 +1,13 @@
 from github import Github, PaginatedList
 
+
 def app() -> None:
-
     print("Hello from DependabotTrigger!")
-    get_pull_requests("JackPlowman/DependabotTrigger")
-    # get_pull_requests("octocat/Hello-World")
+    pulls = get_pull_requests("JackPlowman/DependabotTrigger")
+    close_group_pull_requests(pulls)
 
 
-def get_pull_requests(repo_name:str) -> PaginatedList:
+def get_pull_requests(repo_name: str) -> PaginatedList:
     # Authenticate to GitHub
     github_class = Github()
 
@@ -15,7 +15,7 @@ def get_pull_requests(repo_name:str) -> PaginatedList:
     repo = github_class.get_repo(repo_name)
 
     # Get all pull requests
-    pulls = repo.get_pulls(state='open')
+    pulls = repo.get_pulls(state="open")
 
     print(f"Found {pulls.totalCount} open pull requests in {repo_name}.")
     # Print the title and number of each pull request
@@ -23,3 +23,15 @@ def get_pull_requests(repo_name:str) -> PaginatedList:
         print(f"Pull request #{pull.number}: {pull.title}")
 
     return pulls
+
+
+def close_group_pull_requests(pulls: PaginatedList) -> None:
+    # Close all pull requests in the group
+    count = 0
+    for pull in pulls:
+        if "group" not in pull.title:
+            continue
+        pull.edit(state="closed")
+        count += 1
+        print(f"Closed pull request #{pull.number}: {pull.title}")
+    print(f"Closed {count} pull requests in the group.")
