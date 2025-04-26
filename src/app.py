da67730bccc9bfba7e5cb5f94fdfb5523a2c7eb5
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-04-26 13:13:45 +0100

    update

--- src/app.py
+++ src/app.py
@@ -14,9 +14,10 @@ def app() -> None:
     # Create a Github instance
     github_class = Github(auth_token)
 
-    pulls = get_pull_requests(github_class, "JackPlowman/DependabotTrigger")
+    repo_name = "JackPlowman/DependabotTrigger"
+    pulls = get_pull_requests(github_class, repo_name)
     comment_on_pull_request(pulls)
-
+    log_all_pull_requests(repo_name, pulls)
     github_class.close()
 
 def get_pull_requests(github_class: Github, repo_name: str) -> PaginatedList:
@@ -35,7 +36,7 @@ def get_pull_requests(github_class: Github, repo_name: str) -> PaginatedList:
 
     # Print the title and number of each pull request
     for pull in pulls:
-        logger.info("pull_request", number=pull.number, title=pull.title)
+        logger.debug("pull_request", number=pull.number, title=pull.title)
 
     return pulls
 
@@ -47,3 +48,14 @@ def comment_on_pull_request(pulls: PaginatedList) -> None:
     """
     for pull in pulls:
         pull.create_issue_comment("@dependabot rebase")
+
+def log_all_pull_requests( repo_name: str,
+        pulls: PaginatedList) -> None:
+    """Logs the title and number of each pull request in the provided list.
+
+    Args:
+        repo_name (str): The name of the repository in the format "owner/repo".
+        pulls (PaginatedList): A list of pull request objects to log.
+    """
+    logger.info(repo_name, pull_request_count=pulls.totalCount, pull_request_links=[pull.html_url for pull in pulls])
+
