2593c54e746bb9acc0154922e13d5b7f5a40e098
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-05-04 10:43:34 +0100

    Get Public Repositories (#68)
    # Pull Request
    
    ## Description
    
    This pull request introduces functionality for interacting with the
    GitHub API to retrieve repositories and refactors the Playwright-based
    GitHub login process. It also updates dependencies to support the new
    functionality. Below are the most important changes grouped by theme:
    
    ### GitHub API Integration:
    * Added a new `get_all_repos` function to retrieve a list of
    repositories using the GitHub API. This function authenticates using the
    `GITHUB_TOKEN` environment variable and logs details about the retrieved
    repositories. (`src/app.py`)
    * Imported `Github`, `PaginatedList`, and `Repository` from the
    `pygithub` library to enable interaction with the GitHub API.
    (`src/app.py`)
    
    ### Playwright Enhancements:
    * Refactored the GitHub login process into a new `sign_into_github`
    function, which navigates to the login page and waits for the user to
    complete authentication. (`src/app.py`)
    
    ### Dependency Updates:
    * Added `pygithub==2.6.1` to the `pyproject.toml` dependencies to
    support GitHub API interactions. (`pyproject.toml`)

--- src/app.py
+++ src/app.py
@@ -1,6 +1,9 @@
-from playwright.sync_api import sync_playwright
-
+from playwright.sync_api import sync_playwright, Page
+from os import getenv
 from structlog import get_logger, stdlib
+from github import Github
+from github.PaginatedList import PaginatedList
+from github.Repository import Repository
 
 logger: stdlib.BoundLogger = get_logger()
 
@@ -11,7 +14,34 @@ def app() -> None:
         browser = p.chromium.launch(headless=False)
         context = browser.new_context()
         page = context.new_page()
+        sign_into_github(page)
+        get_all_repos()
 
-        page.goto("https://www.github.com/login")
 
-        page.wait_for_url("https://github.com/", timeout=30000)
+def sign_into_github(page: Page) -> None:
+    """Sign into GitHub using Playwright."""
+    page.goto("https://www.github.com/login")
+    # Wait 30 seconds for user to enter credentials
+    page.wait_for_url("https://github.com/", timeout=30000)
+
+
+def get_all_repos() -> PaginatedList[Repository]:
+    """Retrieve the list of repositories to analyse.
+
+    Returns:
+        PaginatedList[Repository]: The list of repositories.
+    """
+    # Authenticate to GitHub
+    auth_token = getenv("GITHUB_TOKEN")
+    if not auth_token:
+        raise ValueError("GITHUB_TOKEN environment variable not set.")
+    github = Github()
+    repositories = github.search_repositories(
+        query="user:JackPlowman archived:false is:public",
+    )
+    logger.info(
+        "Retrieved repositories to analyse",
+        repositories_count=repositories.totalCount,
+        repositories=[repository.full_name for repository in repositories],
+    )
+    return repositories
